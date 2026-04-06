ea6f5f62b1b931bec1ff91eb1def3fa7f0dc5078
Chorts <centcent765@gmail.com>
2026-04-06 07:53:33 +0700

    Coba 2

--- app/src/main/java/com/example/studentproject/view/StudentListFragment.kt
+++ app/src/main/java/com/example/studentproject/view/StudentListFragment.kt
@@ -40,7 +40,6 @@ class StudentListFragment : Fragment() {
             binding.txtError.visibility = View.GONE
             binding.progressLoad.visibility = View.VISIBLE
             viewModel.refresh()
-            viewModel.refresh()
             binding.refreshLayout.isRefreshing = false
         }
     }
